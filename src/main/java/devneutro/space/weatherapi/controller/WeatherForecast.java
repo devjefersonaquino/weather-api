14e2679ad68d5a0af1cdd51c65f5d35fdd2eee00
Jeferson Aquino <jneris.dev@gmail.com>
2022-08-31 13:00:35 -0300

    Implementação de endpoints

--- src/main/java/devneutro/space/weatherapi/controller/WeatherForecast.java
+++ src/main/java/devneutro/space/weatherapi/controller/WeatherForecast.java
@@ -1,8 +1,6 @@
 package devneutro.space.weatherapi.controller;
 
-import devneutro.space.weatherapi.model.BodyWeather;
-import devneutro.space.weatherapi.model.Main;
-import devneutro.space.weatherapi.model.Weather;
+import devneutro.space.weatherapi.model.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,8 +22,8 @@ public class WeatherForecast {
     @Value("${url.api}")
     private String urlApi;
 
-    @GetMapping("/{city}")
-    public Main getWeather(@PathVariable String city){
+    @GetMapping("main/{city}")
+    public Main getMain(@PathVariable String city){
 
         RestTemplate restTemplate = new RestTemplate();
 
@@ -43,4 +41,141 @@ public class WeatherForecast {
 
         return entity.getBody().getMain();
     }
+
+    @GetMapping("sys/{city}")
+    public Sys getSys(@PathVariable String city){
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        StringBuilder stringBuilder = new StringBuilder();
+        String url = stringBuilder.append(urlApi)
+                .append(city)
+                .append("&appid=")
+                .append(apiKey).toString();
+
+        List<Weather> weathers = new ArrayList<>();
+
+        ResponseEntity<BodyWeather> entity = restTemplate.getForEntity(url, BodyWeather.class);
+
+        System.out.println(entity.getBody().getMain().getTemp());
+
+        return entity.getBody().getSys();
+    }
+    @GetMapping("coord/{city}")
+    public Coord getCoord(@PathVariable String city){
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        StringBuilder stringBuilder = new StringBuilder();
+        String url = stringBuilder.append(urlApi)
+                .append(city)
+                .append("&appid=")
+                .append(apiKey).toString();
+
+        List<Weather> weathers = new ArrayList<>();
+
+        ResponseEntity<BodyWeather> entity = restTemplate.getForEntity(url, BodyWeather.class);
+
+        System.out.println(entity.getBody().getMain().getTemp());
+
+        return entity.getBody().getCoord();
+    }
+    @GetMapping("weather/{city}")
+    public List<Weather> getWeather(@PathVariable String city){
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        StringBuilder stringBuilder = new StringBuilder();
+        String url = stringBuilder.append(urlApi)
+                .append(city)
+                .append("&appid=")
+                .append(apiKey).toString();
+
+        List<Weather> weathers = new ArrayList<>();
+
+        ResponseEntity<BodyWeather> entity = restTemplate.getForEntity(url, BodyWeather.class);
+
+        System.out.println(entity.getBody().getMain().getTemp());
+
+        return entity.getBody().getWeather();
+    }
+
+    @GetMapping("clouds/{city}")
+    public Clouds getClouds(@PathVariable String city){
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        StringBuilder stringBuilder = new StringBuilder();
+        String url = stringBuilder.append(urlApi)
+                .append(city)
+                .append("&appid=")
+                .append(apiKey).toString();
+
+        List<Weather> weathers = new ArrayList<>();
+
+        ResponseEntity<BodyWeather> entity = restTemplate.getForEntity(url, BodyWeather.class);
+
+        System.out.println(entity.getBody().getMain().getTemp());
+
+        return entity.getBody().getClouds();
+    }
+    @GetMapping("rain/{city}")
+    public Rain getRain(@PathVariable String city){
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        StringBuilder stringBuilder = new StringBuilder();
+        String url = stringBuilder.append(urlApi)
+                .append(city)
+                .append("&appid=")
+                .append(apiKey).toString();
+
+        List<Weather> weathers = new ArrayList<>();
+
+        ResponseEntity<BodyWeather> entity = restTemplate.getForEntity(url, BodyWeather.class);
+
+        System.out.println(entity.getBody().getMain().getTemp());
+
+        return entity.getBody().getRain();
+    }
+    @GetMapping("wind/{city}")
+    public Wind getWind(@PathVariable String city){
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        StringBuilder stringBuilder = new StringBuilder();
+        String url = stringBuilder.append(urlApi)
+                .append(city)
+                .append("&appid=")
+                .append(apiKey).toString();
+
+        List<Weather> weathers = new ArrayList<>();
+
+        ResponseEntity<BodyWeather> entity = restTemplate.getForEntity(url, BodyWeather.class);
+
+        System.out.println(entity.getBody().getMain().getTemp());
+
+        return entity.getBody().getWind();
+    }
+
+    @GetMapping("bodyweather/{city}")
+    public BodyWeather getBodyWeather(@PathVariable String city){
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        StringBuilder stringBuilder = new StringBuilder();
+        String url = stringBuilder.append(urlApi)
+                .append(city)
+                .append("&appid=")
+                .append(apiKey).toString();
+
+        List<Weather> weathers = new ArrayList<>();
+
+        ResponseEntity<BodyWeather> entity = restTemplate.getForEntity(url, BodyWeather.class);
+
+        System.out.println(entity.getBody().getMain().getTemp());
+
+        return entity.getBody();
+    }
+
 }
